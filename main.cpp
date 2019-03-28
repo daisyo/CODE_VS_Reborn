69930ab377333c28d20602d42f0856760751196b
daisyo <dsytk7@gmail.com>
2019-03-28 22:45:24 +0900

    init

---
+++ main.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int (i)=(0);(i)<(int)(n);++(i))
+using ll = long long;
+using P = pair<ll, ll>;
+using namespace std;
+
+template<class T> void vin(vector<T>& v, int n) {
+    v.resize(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i];
+    }
+}
+
+int main()
+{
+    // ai name
+    cout << "TEST" << endl;
+    cout.flush();
+
+    rep(i, 500)
+    {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        string tmp;
+        cin >> tmp;
+    }
+    cerr << "A" << endl;
+
+    while (true)
+    {
+        int turn;
+        cin >> turn;
+        cin.ignore();
+        double tm;
+        cin >> tm;
+        cin.ignore();
+        int a, b;
+        cin >> a >> b;
+        cin.ignore();
+        rep(r, 10) rep(c, 16) {
+            int t;
+            cin >> t;
+            cin.ignore();
+        }
+        string tmp;
+        cin >> tmp;
+        cin.ignore();
+
+        int ta, tb, tc;
+        cin >> ta >> tb >> tc;
+        cin.ignore();
+        rep(r, 10) rep(c, 16) {
+            int t;
+            cin >> t;
+            cin.ignore();
+        }
+        cin >> tmp;
+        cout << "3 0" << endl;
+    }
+}
